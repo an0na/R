787d7db7b4062ab0a2cd137e5c32fa8b8ec69f49
Minishifeng <913314010@qq.com>
2020-08-21 12:27:03 +0800

    Update jd_fruit.js

--- JD/jd_fruit.js
+++ JD/jd_fruit.js
@@ -12,9 +12,9 @@ let jdNotify = $.getdata('jdFruitNotify');
 //助力好友分享码(最多4个,否则后面的助力失败),原因:京东农场每人每天只有四次助力机会
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
   '732c806d465d427aab0c948e2ef8de17',
+  '35719fb70a8d4a4aa13effd812661fbf',
   '966c0c23c8594b84882326e032344363',
-  '16dfd4a07a514fd0b70efdb493d82be7',
-  '35719fb70a8d4a4aa13effd812661fbf'
+  '16dfd4a07a514fd0b70efdb493d82be7'
 ]
 // 添加box功能
 // 【用box订阅的好处】
